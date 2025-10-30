ba72ed4ab02f56899c37ef1ef94588e29d3514a4
Florian Chague <florian.chague2@gmail.com>
2025-10-30 10:30:24 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <PokemonBadge />
+  <Meteo />
+</template>
+
+<script setup>
+import PokemonBadge from './components/PokemonBadge.vue';
+import Meteo from './components/Meteo.vue';
+</script>
